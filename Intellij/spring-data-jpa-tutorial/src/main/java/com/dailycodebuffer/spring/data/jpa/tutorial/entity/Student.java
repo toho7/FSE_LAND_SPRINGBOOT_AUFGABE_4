d542d4a344c55502d5f94b5cd8249eddb34332a2
Thomas Hofmann <thofmann2104@gmail.com>
2021-11-12 21:13:16 +0100

    Erstellung Projekt, Verbindung zur DB, erste Entität

---
+++ Intellij/spring-data-jpa-tutorial/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/Student.java
@@ -0,0 +1,27 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+
+
+}
