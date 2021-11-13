9730856658fe9333e2ef3ae1bc95e4dad99b4e34
Thomas Hofmann <thofmann2104@gmail.com>
2021-11-13 12:53:02 +0100

    Fetch Types

--- Intellij/spring-data-jpa-tutorial/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/CourseMaterial.java
+++ Intellij/spring-data-jpa-tutorial/src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/CourseMaterial.java
@@ -1,9 +1,6 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -12,6 +9,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ToString(exclude = "course")
 public class CourseMaterial {
 
     @Id
@@ -27,7 +25,8 @@ public class CourseMaterial {
     private String url;
 
     @OneToOne (
-            cascade = CascadeType.ALL)
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
     @JoinColumn(
             name = "course_id",
             referencedColumnName = "courseId")
